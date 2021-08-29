a2e9fb609537da88f5e05a06b71c60e050b8ee8d
CheungYx <1373683140@qq.com>
2021-08-29 17:19:22 +0800

    second commit

--- src/main/java/com/cheungyx/git/GitTest.java
+++ src/main/java/com/cheungyx/git/GitTest.java
@@ -10,5 +10,6 @@ package com.cheungyx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
