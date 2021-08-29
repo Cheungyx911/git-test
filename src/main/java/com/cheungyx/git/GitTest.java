3fc00e04a17b2541faebf6dcfc9f0caf9f994d10
CheungYx <1373683140@qq.com>
2021-08-29 17:44:04 +0800

    push commit

--- src/main/java/com/cheungyx/git/GitTest.java
+++ src/main/java/com/cheungyx/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hello hotfix");
         System.out.println("hello master");
         System.out.println("hello hotfix2");
+        System.out.println("push test");
     }
 }
