70b44acd44183a55233c27d3b96405af076970c4
Cheungyx911 <60864632+Cheungyx911@users.noreply.github.com>
2021-08-29 18:03:30 +0800

    pull commit

--- src/main/java/com/cheungyx/git/GitTest.java
+++ src/main/java/com/cheungyx/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hotfix2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
