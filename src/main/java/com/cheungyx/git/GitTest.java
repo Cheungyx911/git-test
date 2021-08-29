2e1263516009365a1c1a174f85e287db89efdda2
CheungYx <1373683140@qq.com>
2021-08-29 17:17:49 +0800

    first commit

---
+++ src/main/java/com/cheungyx/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.cheungyx.git;
+
+/**
+ * @author xiaozhang
+ * @date 2021-08-29 17:14
+ * @description:
+ * @version:
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
